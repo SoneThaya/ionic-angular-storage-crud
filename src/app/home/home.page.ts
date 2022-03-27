8ec9c0ee5dc6f659d3db28b698953a7b36ad592f
sone <sthaya@gmail.com>
2022-03-26 20:42:33 -0700

    ionic angular crud with local storage

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { ListenerCallback } from '@capacitor/core';
+import { StorageService, Item } from '../services/storage.service';
+import { IonList, Platform, ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +9,66 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  items: Item[] = [];
 
-  constructor() {}
+  newItem: Item = <Item>{};
 
+  @ViewChild('mylist') mylist: IonList;
+
+  constructor(
+    private storageService: StorageService,
+    private plt: Platform,
+    private toastController: ToastController
+  ) {
+    this.plt.ready().then(() => {
+      this.loadItems();
+    });
+  }
+
+  // create
+  addItem() {
+    this.newItem.modified = Date.now();
+    this.newItem.id = Date.now();
+
+    this.storageService.addItem(this.newItem).then((item) => {
+      this.newItem = <Item>{};
+      this.showToast('Item added!');
+      this.loadItems(); // or add it to the array directly
+    });
+  }
+
+  // read
+  loadItems() {
+    this.storageService.getItems().then((items) => {
+      this.items = items;
+    });
+  }
+
+  // update
+  updateItem(item: Item) {
+    item.title = `UPDATED: ${item.title}`;
+    item.modified = Date.now();
+
+    this.storageService.updateItem(item).then((item) => {
+      this.showToast('Item updated!');
+    });
+  }
+
+  // delete
+  deleteItem(item: Item) {
+    this.storageService.deleteItem(item.id).then((item) => {
+      this.showToast('Item removed!');
+      this.mylist.closeSlidingItems(); // fix or sliding is stuck afterwards
+      this.loadItems(); // or splice it from the array directly
+    });
+  }
+
+  // helper
+  async showToast(msg) {
+    const toast = await this.toastController.create({
+      message: msg,
+      duration: 2000,
+    });
+    toast.present();
+  }
 }
